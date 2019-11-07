e30929514868c79b97bf3213ba72f92cc0c4037c
tranvantri170399 <51941534+tranvantri170399@users.noreply.github.com>
2019-11-07 20:28:01 +0700

    moi1

---
+++ tranvantri/src/tranvantri/Tranvantri.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tranvantri;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class Tranvantri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                Scanner in =  new Scanner(System.in);
+        // TODO code application logic here
+        System.out.println("tranvantri");
+        System.out.println("nhap a: ");
+        int i;
+       i= in.nextInt();
+
+           System.out.println("so vua nhap la: "+i);  
+    }
+    
+}
